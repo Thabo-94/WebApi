eeba4738eaa232a48b91c7205510e332f1d91c03
Thabo Matue <ThaboMat@statssa.gov.za>
2023-08-10 08:23:33 +0200

    initial commit

---
+++ WebApi/Demo.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi
+{
+    public class Demo
+    {
+        public string Name { get; init => field = value.Trim(); }
+        public DateTime DateOfBirth { get; set => field = value.Date; }
+    }
+}
+
